9efeecb1ddecdaea765c7d1389fb5a765cd24899
Kike Rodriguez <enriquemondejar15@gmail.com>
2025-04-19 11:18:56 +0200

    new style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog de Noticias';
-export const SITE_DESCRIPTION = 'Blog de Noticias';
+export const SITE_TITLE = 'Noticias Nundusoft';
+export const SITE_DESCRIPTION = 'Blog de noticias sobre finanzas, tecnología y el sector financiero por Nundusoft';
